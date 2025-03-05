773ffc1e72b266817a381c0f24a1ecfa047e3773
Ростислав Ермолаев <0988560505r@gmail.com>
2025-03-05 10:45:32 +0200

    Initial commit

--- components/users/users.controller.js
+++ components/users/users.controller.js
@@ -3,14 +3,14 @@ const ErrorResponse = require('../../utils/errorResponse')
 const User = require('./user.model')
 
 // @desc    Get all users
-// @route   GET /api/v1/auth/users
+// @route   GET /api/v1/users
 // @access  Private/Admin
 exports.getUsers = asyncHandler(async (req, res, next) => {
 	res.status(200).json(res.advancedResults)
 })
 
 // @desc    Get single user
-// @route   GET /api/v1/auth/users/:id
+// @route   GET /api/v1/users/:id
 // @access  Private/Admin
 exports.getUser = asyncHandler(async (req, res, next) => {
 	const user = await User.findById(req.params.id)
